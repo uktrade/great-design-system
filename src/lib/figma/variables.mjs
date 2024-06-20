6bf20bdd456e451438ebf0255372caae4aaee180
boxadesign <info@boxadesign.co.uk>
2024-06-20 12:22:27 +0100

    Remove comment

--- src/lib/figma/variables.mjs
+++ src/lib/figma/variables.mjs
@@ -5,7 +5,6 @@ import { fileURLToPath } from "url";
 import { diffLines } from "diff";
 import Table from "cli-table3";
 
-// Create __dirname equivalent for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
