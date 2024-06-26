153445d598b70941802f17b81ce6b0be5a562678
boxadesign <info@boxadesign.co.uk>
2024-06-26 09:38:09 +0100

    Update main.scss import paths to govuk-frontend

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,67 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const sass = require('sass');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const sass = require("sass");
 
 module.exports = {
   entry: {
-    main: ['./src/js/main.js', './src/scss/main.scss']
+    main: ["./src/js/main.js", "./src/scss/main.scss"],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { 
-            loader: 'css-loader', 
-            options: { 
+          {
+            loader: "css-loader",
+            options: {
               sourceMap: true,
               url: false,
-            }
+            },
           },
-          { 
-            loader: 'resolve-url-loader', 
-            options: { 
+          {
+            loader: "resolve-url-loader",
+            options: {
               sourceMap: true,
-            } 
+            },
           },
-          { 
-            loader: 'sass-loader', 
-            options: { 
+          {
+            loader: "sass-loader",
+            options: {
               implementation: sass,
               sourceMap: true,
               sassOptions: {
-                outputStyle: 'compressed',
-                includePaths: ['node_modules'],
+                outputStyle: "compressed",
+                includePaths: ["node_modules"],
               },
             },
           },
-        ]
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
             sourceMaps: true,
-          }
-        }
+          },
+        },
       },
-    ]
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css'
+      filename: "css/[name].css",
     }),
   ],
   stats: {
     errorDetails: true,
-  }
+  },
 };
