9a47ec56a28c92e9571d974b8be093546c039220
Oliver Weare <oliver_weare@protonmail.com>
2025-03-03 09:01:19 +0000

    Basic copy url functionality

--- src/js/copy-url/copyUrl.js
+++ src/js/copy-url/copyUrl.js
@@ -1,1 +1,8 @@
-export const copyUrlToClipboard = (element) => {};
+export const copyUrlToClipboard = async (event) => {
+  try {
+    const url = window.location.href;
+    await navigator.clipboard.writeText(url);
+  } catch (error) {
+    console.log(error);
+  }
+};
