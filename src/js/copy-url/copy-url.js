54161e1d5a6df26e89aa15c02d72a3df82ace707
Oliver Weare <oliver_weare@protonmail.com>
2025-03-03 14:52:28 +0000

    Use correct file naming convention for project

--- src/js/copy-url/copy-url.js
+++ src/js/copy-url/copy-url.js
@@ -1,7 +1,8 @@
-export const copyUrlToClipboard = async (event) => {
+export const copyUrlToClipboard = async () => {
   try {
     const url = window.location.href;
     await navigator.clipboard.writeText(url);
+    console.log("URL copied");
   } catch (error) {
     console.log(error);
   }
