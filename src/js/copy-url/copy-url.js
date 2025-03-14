3e260633f5ab885f9cf61d03b1089fd417f2c65d
Oliver Weare <oliver_weare@protonmail.com>
2025-03-14 15:54:39 +0000

    Remove console log

--- src/js/copy-url/copy-url.js
+++ src/js/copy-url/copy-url.js
@@ -2,7 +2,6 @@ export const copyURLToClipboard = async () => {
   try {
     const url = window.location.href;
     await navigator.clipboard.writeText(url);
-    console.log("URL copied");
   } catch (error) {
     console.error("Failed to copy URL: ", error);
   }
